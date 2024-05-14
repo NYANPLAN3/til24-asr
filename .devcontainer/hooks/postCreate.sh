276e2e1b179227a5e402a2642fa5e43b319213ae
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-05-14 16:25:09 +0000

    fix: add whisper cache

--- .devcontainer/hooks/postCreate.sh
+++ .devcontainer/hooks/postCreate.sh
@@ -7,5 +7,6 @@ poetry config virtualenvs.prompt venv
 poetry install
 
 sudo chown -R vscode:vscode /home/vscode/.cache/pypoetry
+sudo chown -R vscode:vscode /home/vscode/.cache/whisper
 
 # /workspaces/til24-asr/.devcontainer/hooks/setupGCS.sh
