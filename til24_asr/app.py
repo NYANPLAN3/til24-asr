ce3670aa94d45e6af6e7c27f567faee3d379b7b2
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-06-01 21:52:22 +0000

    feat: record WER per transcript for easier debugging

--- til24_asr/app.py
+++ til24_asr/app.py
@@ -46,7 +46,8 @@ def create_app():
             import torch  # type: ignore
 
             debug["torch_version"] = torch.__version__
-            debug["cuda_device"] = str(torch.zeros([10, 10], device="cuda").device)
+            debug["cuda_device"] = str(
+                torch.zeros([10, 10], device="cuda").device)
         except ImportError:
             pass
 
@@ -77,54 +78,60 @@ def create_app():
 
         return re.sub(r"(^|[.!?]\s+)([a-z])", capitalize_match, text)
 
-    def process_output(output):
+    # fmt: off
+    def process_output(o):
         # Add a space between a digit and a letter / letter and a digit
-        output = re.sub(r"(?<=\d)(?=[a-zA-Z])", " ", output)
-        output = re.sub(r"(?<=[a-zA-Z])(?=\d)", " ", output)
-        output = re.sub(r"(?<=\d)(?=\d)", " ", output)
+        o = re.sub(r"(?<=\d)(?=[a-zA-Z])", " ", o)
+        o = re.sub(r"(?<=[a-zA-Z])(?=\d)", " ", o)
+        o = re.sub(r"(?<=\d)(?=\d)", " ", o) # space between digits
 
         # Add period at the end of text
-        output = re.sub(r"([^.,])([.,])$", r"\1.", output)
-        output = re.sub(r"(?<=\w)\.(?=\w)", " ", output)
+        o = re.sub(r"([^.,])([.,])$", r"\1.", o)
+        o = re.sub(r"(?<=\w)\.(?=\w)", " ", o)
+
+        # Correct some number stuff before converting to words
+        # o = re.sub(r" +", " ", o)
+        # o = re.sub(r"\bheading 2 (\d \d \d)\b", r"heading to \1", o, flags=re.IGNORECASE)
 
         # Numbers to words
-        output = re.sub(r"(\d+)", lambda m: num2words(int(m.group())), output)
-        
-        output = re.sub(r"\b(ground)\b", r"brown", output, flags=re.IGNORECASE)
-        output = re.sub(r"\b(machine guns)\b", r"machine gun", output, flags=re.IGNORECASE)
-        output = re.sub(r"\b(fighter jets)\b", r"fighter jet", output, flags=re.IGNORECASE)
-        output = re.sub(r"\b(capture)\b", r"catcher", output, flags=re.IGNORECASE)
-        output = re.sub(r"\b(nine)\b", r"niner", output, flags=re.IGNORECASE)
-        output = re.sub(r"\b(torret)\b", r"turret", output, flags=re.IGNORECASE)
-        output = re.sub(r"\b(torrid)\b", r"turret", output, flags=re.IGNORECASE)
-        output = re.sub(r"\b(turrell)\b", r"turret", output, flags=re.IGNORECASE)
-        output = re.sub(r"\b(engate)\b", r"engage", output, flags=re.IGNORECASE)
-        output = re.sub(r"\b(gray)\b", r"grey", output, flags=re.IGNORECASE)
-        output = re.sub(
-            r"\b(intercepted)\b", r"interceptor", output, flags=re.IGNORECASE
-        )
-        output = re.sub(r"\b(engaged)\b", r"engage", output, flags=re.IGNORECASE)
-        output = re.sub(r"\b(hostel)\b", r"hostile", output, flags=re.IGNORECASE)
-        output = re.sub(r"\b(Heading)\b", r"heading", output)
-        output = re.sub(r"\b(anterior)\b", r"anti-air", output, flags=re.IGNORECASE)
-        output = re.sub(r"\b(anti air)\b", r"anti-air", output, flags=re.IGNORECASE)
-        output = re.sub(
-            r"\b(surface to air)\b", r"surface-to-air", output, flags=re.IGNORECASE
-        )
+        o = re.sub(r"(\d+)", lambda m: num2words(int(m.group())), o)
+
+        o = re.sub(r"\b(ground)\b", r"brown", o, flags=re.IGNORECASE)
+        o = re.sub(r"\b(machine guns)\b", r"machine gun", o, flags=re.IGNORECASE)
+        o = re.sub(r"\b(fighter jets)\b", r"fighter jet", o, flags=re.IGNORECASE)
+        o = re.sub(r"\b(capture)\b", r"catcher", o, flags=re.IGNORECASE)
+        o = re.sub(r"\b(nine)\b", r"niner", o, flags=re.IGNORECASE)
+        o = re.sub(r"\b(torret)\b", r"turret", o, flags=re.IGNORECASE)
+        o = re.sub(r"\b(torrid)\b", r"turret", o, flags=re.IGNORECASE)
+        o = re.sub(r"\b(turrell)\b", r"turret", o, flags=re.IGNORECASE)
+        o = re.sub(r"\b(engate)\b", r"engage", o, flags=re.IGNORECASE)
+        o = re.sub(r"\b(gray)\b", r"grey", o, flags=re.IGNORECASE)
+        o = re.sub(r"\b(intercepted)\b", r"interceptor", o, flags=re.IGNORECASE)
+        o = re.sub(r"\b(engaged)\b", r"engage", o, flags=re.IGNORECASE)
+        o = re.sub(r"\b(hostel)\b", r"hostile", o, flags=re.IGNORECASE)
+        o = re.sub(r"\b(Heading)\b", r"heading", o)
+        o = re.sub(r"\b(anterior)\b", r"anti-air", o, flags=re.IGNORECASE)
+        o = re.sub(r"\b(anti air)\b", r"anti-air", o, flags=re.IGNORECASE)
+        o = re.sub(r"\b(surface to air)\b", r"surface-to-air", o, flags=re.IGNORECASE)
+        # idk
+        o = re.sub(r"\b(great)\b", r"red", o, flags=re.IGNORECASE)
+        o = re.sub(r"\b(grate)\b", r"red", o, flags=re.IGNORECASE)
+        o = re.sub(r"\b(rate)\b", r"red", o, flags=re.IGNORECASE)
 
         # US English to UK English
         # output = us_spelling_to_uk(output)
 
         # Capitalize first letter
-        output = capitalize_start_of_sentence(output)
+        o = capitalize_start_of_sentence(o)
 
         # Remove extra spaces
-        output = re.sub(r" +", " ", output)
-        output = output.strip()
-        output = output[0].upper() + output[1:]
+        o = re.sub(r" +", " ", o)
+        o = o.strip()
+        o = o[0].upper() + o[1:]
 
-        return output
+        return o
 
+    # fmt: on
     @app.post("/stt")
     async def stt(req: STTRequest):
         """Performs ASR given the filepath of an audio file."""
